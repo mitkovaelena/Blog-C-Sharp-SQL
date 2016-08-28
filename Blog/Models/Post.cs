385e9b70e5c8409b2193f3a6407c669b3cd0e888
mitkovaelena <mitkova_elena@yahoo.com>
2016-08-28 21:25:49 +0300

    more fixes

--- Blog/Models/Post.cs
+++ Blog/Models/Post.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -21,6 +22,7 @@ namespace Blog.Models
         public string Title { get; set; }
 
         [Required]
+        [DataType(DataType.MultilineText)]
         public string Body { get; set; }
 
         [Required]
